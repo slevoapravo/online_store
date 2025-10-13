cc0f2023b4f7d01033dee50e38e53bf4e56d2747
Your Name <you@example.com>
2025-10-13 16:45:52 +0300

    4

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,7 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # path('home/', views.home, name='home'),  #<-- home.html поставлена за главную страницу
     path('contacts/', views.contacts, name='contacts'),
+    path('products/<int:product_id>', views.product_details, name='product_details'),
+    path('to_add_product/', views.to_add_product, name='to_add_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Product, Category
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {
+        'prods': products
+    }
+    return render(request, 'catalog/home.html', context=context)
 
 
 def contacts(request):
@@ -15,3 +21,40 @@ def contacts(request):
 
         return HttpResponse(f'Спасибо {name}, с номером "{phone}" и сообщением "{message}". Данные в сохранность!)')
     return render(request, 'catalog/contacts.html')
+
+
+def product_details(request, product_id):
+
+    product = get_object_or_404(Product, id=product_id)
+    context = {
+        'product': product
+    }
+
+    return render(request, 'catalog/product_details.html', context=context)
+
+
+def to_add_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        category_id = request.POST.get('category')
+        image = request.FILES.get('image')
+
+        category = Category.objects.get(id=category_id)
+
+        Product.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            category=category,
+            image=image
+        )
+
+        return redirect('home')
+
+    categories = Category.objects.all()
+    context = {
+        "categories": categories
+    }
+    return render(request, 'catalog/to_add_product.html', context=context)
