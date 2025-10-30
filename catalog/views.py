85a28aa9e94f7f492aa403f4d598e0529e6edae9
Your Name <you@example.com>
2025-10-30 16:21:36 +0300

    forms

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from .forms import ProductForm
 
 
 class ProductListView(ListView):
@@ -20,7 +21,7 @@ class ProductDetailView(DetailView):
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ['name', 'description', 'price', 'category', 'image']
+    form_class = ProductForm
     template_name = 'catalog/product_form.html'
     extra_context = {'categories': Category.objects.all()}
     success_url = reverse_lazy('catalog:home')
@@ -28,7 +29,7 @@ class ProductUpdateView(UpdateView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ['name', 'description', 'price', 'category', 'image']
+    form_class = ProductForm
     extra_context = {'categories': Category.objects.all()}
     success_url = reverse_lazy('catalog:home')
 
@@ -53,3 +54,57 @@ class ContactView(View):
         print(f'Спасибо {name}, с номером "{phone}" и сообщением "{message}". Данные в сохранность!')
 
         return HttpResponseRedirect(reverse_lazy('catalog:home'))
+
+# def home(request):
+#     products = Product.objects.all()
+#     context = {
+#         'prods': products
+#     }
+#     return render(request, 'catalog/home.html', context=context)
+
+
+# def contacts(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#
+#         return HttpResponse(f'Спасибо {name}, с номером "{phone}" и сообщением "{message}". Данные в сохранность!)')
+#     return render(request, 'catalog/contacts.html')
+
+
+# def product_details(request, product_id):
+#
+#     product = get_object_or_404(Product, id=product_id)
+#     context = {
+#         'product': product
+#     }
+#
+#     return render(request, 'catalog/product_detail.html', context=context)
+
+
+# def to_add_product(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         description = request.POST.get('description')
+#         price = request.POST.get('price')
+#         category_id = request.POST.get('category')
+#         image = request.FILES.get('image')
+#
+#         category = Category.objects.get(id=category_id)
+#
+#         Product.objects.create(
+#             name=name,
+#             description=description,
+#             price=price,
+#             category=category,
+#             image=image
+#         )
+#
+#         return redirect('home')
+#
+#     categories = Category.objects.all()
+#     context = {
+#         "categories": categories
+#     }
+#     return render(request, 'catalog/product_form.html', context=context)
